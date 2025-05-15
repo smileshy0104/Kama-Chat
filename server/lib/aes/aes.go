3b37f8ae8bf7f24af577b8d0a80cf0afbe1402bd
yuyansong <1259581033@qq.com>
2025-05-16 07:59:26 +0800

    kafka相关内容

--- server/lib/aes/aes.go
+++ server/lib/aes/aes.go
@@ -6,17 +6,27 @@ import (
 	"encoding/base64"
 )
 
+// encryptAES 使用AES算法加密数据。
+// 该函数需要三个参数：待加密的数据(data)、加密密钥(key)和初始化向量(iv)。
+// 所有参数都以字节切片的形式提供。
+// 函数返回加密后的数据，以Base64编码的字符串形式输出，以及一个错误对象。
 func encryptAES(data, key, iv []byte) (string, error) {
+	// 创建AES密钥对应的加密器。
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
 
+	// 创建一个足够大的字节切片来存储加密后的数据，包括初始化向量的长度。
 	ciphertext := make([]byte, aes.BlockSize+len(data))
+	// 复制初始化向量到一个新的切片，以避免修改原始的初始化向量。
 	ivCopy := make([]byte, aes.BlockSize)
 	copy(ivCopy, iv)
+	// 创建一个基于CFB模式的加密器。
 	stream := cipher.NewCFBEncrypter(block, ivCopy)
+	// 使用加密器对数据进行加密。
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], data)
 
+	// 将初始化向量和加密后的数据合并，并以Base64编码，然后返回编码后的字符串。
 	return base64.StdEncoding.EncodeToString(append(ivCopy, ciphertext[aes.BlockSize:]...)), nil
 }
--- server/lib/kafka/kafka.go
+++ server/lib/kafka/kafka.go
@@ -4,6 +4,7 @@ import (
 	"Kama-Chat/global"
 	"Kama-Chat/initialize/zlog"
 	"context"
+	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
 	"time"
 )
@@ -13,39 +14,49 @@ var ctx = context.Background()
 
 // kafkaService 结构体定义了 Kafka 相关的服务
 type kafkaService struct {
-	ChatWriter *kafka.Writer
-	ChatReader *kafka.Reader
-	KafkaConn  *kafka.Conn
+	ChatWriter *kafka.Writer // ChatWriter 是用于发送消息的 Kafka Writer
+	ChatReader *kafka.Reader // ChatReader 是用于接收消息的 Kafka Reader
+	KafkaConn  *kafka.Conn   // KafkaConn 是用于创建 topic 的 Kafka 连接
+	Ctx        *gin.Context  // Ctx 是 gin 上下文，用于在处理请求时传递上下文信息
 }
 
 // KafkaService 是 kafkaService 类型的全局实例
 var KafkaService = new(kafkaService)
 
-// KafkaInit 初始化 Kafka 服务
-// 该方法配置了 ChatWriter 和 ChatReader，为之后的消息发送和接收做准备
+// KafkaInit 初始化 Kafka 服务。
+// 该函数根据 global.CONFIG.KafkaConfig 中的配置设置 Kafka 的写入器和读取器。
+// 不返回任何值。
 func (k *kafkaService) KafkaInit() {
-	//k.CreateTopic()
+	// 获取全局配置中的 Kafka 配置
 	kafkaConfig := global.CONFIG.KafkaConfig
+
+	// 配置并初始化 Kafka 写入器，用于发送消息
+	// 使用 Hash 负载均衡策略分配分区
+	// 设置写入超时时间、无需确认机制，并禁用自动创建 Topic 功能
 	k.ChatWriter = &kafka.Writer{
-		Addr:                   kafka.TCP(kafkaConfig.HostPort),
-		Topic:                  kafkaConfig.ChatTopic,
-		Balancer:               &kafka.Hash{},
-		WriteTimeout:           kafkaConfig.Timeout * time.Second,
-		RequiredAcks:           kafka.RequireNone,
-		AllowAutoTopicCreation: false,
+		Addr:                   kafka.TCP(kafkaConfig.HostPort),   // Kafka Broker 地址
+		Topic:                  kafkaConfig.ChatTopic,             // 发送消息的 Topic 名称
+		Balancer:               &kafka.Hash{},                     // 使用 Hash 负载均衡策略分配分区
+		WriteTimeout:           kafkaConfig.Timeout * time.Second, // 写入超时时间
+		RequiredAcks:           kafka.RequireNone,                 // 不需要 broker 确认
+		AllowAutoTopicCreation: false,                             // 禁止自动创建 Topic
 	}
+
+	// 配置并初始化 Kafka 读取器，用于接收消息
+	// 设置 Broker 地址、监听的 Topic、提交间隔、消费者组 ID 和起始偏移量
 	k.ChatReader = kafka.NewReader(kafka.ReaderConfig{
-		Brokers:        []string{kafkaConfig.HostPort},
-		Topic:          kafkaConfig.ChatTopic,
-		CommitInterval: kafkaConfig.Timeout * time.Second,
-		GroupID:        "chat",
-		StartOffset:    kafka.LastOffset,
+		Brokers:        []string{kafkaConfig.HostPort},    // Kafka Broker 地址
+		Topic:          kafkaConfig.ChatTopic,             // 消费的 Topic 名称
+		CommitInterval: kafkaConfig.Timeout * time.Second, // 提交偏移量的时间间隔
+		GroupID:        "chat",                            // 消费者组 ID
+		StartOffset:    kafka.LastOffset,                  // 从最后一条消息开始消费
 	})
 }
 
 // KafkaClose 关闭 Kafka 连接
 // 该方法确保在服务停止时，正确关闭 ChatWriter 和 ChatReader
 func (k *kafkaService) KafkaClose() {
+	// 关闭 ChatWriter 和 ChatReader
 	if err := k.ChatWriter.Close(); err != nil {
 		zlog.Error(err.Error())
 	}
@@ -60,20 +71,23 @@ func (k *kafkaService) CreateTopic() {
 	// 如果已经有topic了，就不创建了
 	kafkaConfig := global.CONFIG.KafkaConfig
 
+	// 获取全局配置中的 Kafka 配置ChatTopic
 	chatTopic := kafkaConfig.ChatTopic
 
 	// 连接至任意kafka节点
 	var err error
+	// 连接至kafka节点
 	k.KafkaConn, err = kafka.Dial("tcp", kafkaConfig.HostPort)
 	if err != nil {
 		zlog.Error(err.Error())
 	}
 
+	// 创建topic的配置
 	topicConfigs := []kafka.TopicConfig{
 		{
-			Topic:             chatTopic,
-			NumPartitions:     kafkaConfig.Partition,
-			ReplicationFactor: 1,
+			Topic:             chatTopic,             // topic名称
+			NumPartitions:     kafkaConfig.Partition, // 分区数
+			ReplicationFactor: 1,                     // 副本因子
 		},
 	}
 
@@ -81,5 +95,4 @@ func (k *kafkaService) CreateTopic() {
 	if err = k.KafkaConn.CreateTopics(topicConfigs...); err != nil {
 		zlog.Error(err.Error())
 	}
-
 }
